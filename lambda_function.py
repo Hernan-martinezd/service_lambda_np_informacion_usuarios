9998c9375a1391c6b1ad5a0a1f2159f766a504e8
Hernan <hd.martinezd1@uniandes.edu.co>
2024-02-20 22:36:08 -0500

    Code version 1 added

--- lambda_function.py
+++ lambda_function.py
@@ -10,5 +10,22 @@ app = Flask(__name__)
 def intex():
     return jsonify(status=200, message='OK')
 
+@app.router('/monitor')
+def monitor():
+    return jsonify(status= 200, message= 'Echo: I\'m good')
+
+@app.router('/health')
+def health_data():
+    health_metrics = {
+        "frecuencia cardíaca": 72,  # bpm 
+        "ftp": 250,                 # Functional Threshold Power en watts 
+        "saturación": 98,           # Porcentaje de saturación de oxígeno
+        "velocidad": 5.5,           # km/h
+        "distancia recorrida": 10,2 # Km
+    }
+
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
+
+if __name__ == "__main__":
+    app.run()

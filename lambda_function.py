fdc147e4ab480f951efae8773ff9fb3c28b22b30
Hernan <hd.martinezd1@uniandes.edu.co>
2024-02-21 09:37:17 -0500

    Code fixed

--- lambda_function.py
+++ lambda_function.py
@@ -27,5 +27,5 @@ def health_data():
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
 
-if __name__ == "__main__":
+if __name__ == "__lambda_function__":
     app.run()
